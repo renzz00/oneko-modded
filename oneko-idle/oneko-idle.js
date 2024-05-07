6b3a4ee40ba8551dcbcb316f12d451f0bde3e9f3
renzz <robbiepstanley0@gmail.com>
2024-05-06 23:41:47 -0400

    Update oneko-idle.js

--- oneko-idle/oneko-idle.js
+++ oneko-idle/oneko-idle.js
@@ -1,6 +1,10 @@
 // THIS IS A MODDED VERSION OF ONEKO BY renzz00. DO NOT EDIT UNLESS YOU KNOW WHAT YOU ARE DOING!
 // oneko.js: https://github.com/adryd325/oneko.js
 
+window.onload = function(){
+  draggable('one');
+};
+
 (function oneko() {
     const nekoEl = document.createElement("div");
   
@@ -92,8 +96,8 @@
       nekoEl.ariaHidden = true;
       nekoEl.style.width = "32px";
       nekoEl.style.height = "32px";
-      nekoEl.style.position = "fixed";
-      nekoEl.style.pointerEvents = "none";
+      nekoEl.style.position = "static";
+      nekoEl.style.pointerEvents = "auto";
       nekoEl.style.backgroundImage = "url('./oneko.gif')";
       nekoEl.style.imageRendering = "pixelated";
       nekoEl.style.left = `${nekoPosX - 16}px`;
@@ -235,47 +239,27 @@
     init();
   })();
 
-  // grab function
-
-   dragElement(document.getElementById("oneko"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+var dragObj = null;
+function draggable(id)
+{
+    var obj = document.getElementById(id);
+    obj.style.position = "absolute";
+    obj.onmousedown = function(){
+            dragObj = obj;
+    }
 }
+
+document.onmouseup = function(e){
+    dragObj = null;
+};
+
+document.onmousemove = function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+
+    if(dragObj == null)
+        return;
+
+    dragObj.style.left = x +"px";
+    dragObj.style.top= y +"px";
+};
