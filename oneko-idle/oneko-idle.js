95ea9d0fc5a47868d2b552b88d9f992e5e153e38
renzz <148696025+renzz00@users.noreply.github.com>
2024-05-06 22:44:20 -0400

    Update oneko-idle.js

--- oneko-idle/oneko-idle.js
+++ oneko-idle/oneko-idle.js
@@ -103,8 +103,8 @@
     document.body.appendChild(nekoEl);
 
     document.addEventListener("mousemove", function (event) {
-      mousePosX = 0;
-      mousePosY = 0;
+      mousePosX = 360;
+      mousePosY = 970;
     });
     
     window.requestAnimationFrame(onAnimatonFrame);
